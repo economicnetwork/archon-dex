5121f4c96be551e04d6dd3bc7077081d8712e024
benjyz <ben@enet.io>
2019-02-11 12:29:40 +0700

    signing issue

--- radar_create_order.py
+++ radar_create_order.py
@@ -2,20 +2,24 @@
 submit order to radar with web3py and infura
 """
 
-from zero_ex.order_utils import generate_order_hash_hex
+from zero_ex.order_utils import generate_order_hash_hex, Order, jsdict_order_to_struct, order_to_jsdict
+from zero_ex.order_utils.asset_data_utils import encode_erc20_asset_data
 from pymaker.zrxv2 import ZrxExchangeV2, ZrxRelayerApiV2, ERC20Asset
 import requests
+from typing import cast, Dict, NamedTuple, Tuple
 from pymaker.sign import eth_sign, to_vrs
 from pymaker.util import bytes_to_hexstring, hexstring_to_bytes, http_response_summary
 from web3 import Web3, HTTPProvider
 from solc import compile_source
 from eth_account.messages import defunct_hash_message
 from pymaker import Wad, Address
+from zero_ex.order_utils import asset_data_utils as adu
 #from eth_account import Account
 
 import os
 import copy
 import json
+
 
 privateKey = os.environ['PRIVATEKEY']
 INFURA_KEY = os.environ['INFURA_KEY']
@@ -39,16 +43,7 @@ def request_order():
   #print ("order ",order)
   return order
 
-def signature_order(order):  
-    print (order['makerAssetData'])
-    print (order['takerAssetData'])
-    #order['makerAssetData'] = hexstring_to_bytes(order['makerAssetData'])
-    #order['takerAssetData'] = hexstring_to_bytes(order['takerAssetData'])
-    order['makerAssetData'] = hexstring_to_bytes("0xf47261b0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
-    order["takerAssetData"] = hexstring_to_bytes("0xf47261b00000000000000000000000001985365e9f78359a9b6ad760e32412f4a445e862")
-    exchangeAddress = order["exchangeAddress"]
-    #order['makerAssetData'] = order['makerAssetData'].to_bytes(1, byteorder='big') * 20
-
+def sign_order(order):
     order_hash = "0x" + generate_order_hash_hex(order, exchangeAddress)
     print ("order_hash ",order_hash)
     orderhash_bytes = hexstring_to_bytes(order_hash)
@@ -56,8 +51,6 @@ def signature_order(order):
     message_hash = defunct_hash_message(primitive=msg)
     sighex = acct.signHash(message_hash).signature.hex()
     v, r, s = to_vrs(sighex)
-    #signed_order = copy.copy(order)
-    #print (signed_order)
     osig = bytes_to_hexstring(bytes([v])) + \
                               bytes_to_hexstring(r)[2:] + \
                               bytes_to_hexstring(s)[2:] + \
@@ -65,60 +58,46 @@ def signature_order(order):
     print ("sig ",osig)                              
     return osig  
 
-def sign_order(order):
-    #self.get_order_hash(order)
-    #exchange_address=exchangeAddress
-    exchangeAddress = order["exchangeAddress"]
-    order_hash = "0x" + generate_order_hash_hex(order, exchange_address)
-    #print ("order_hash ",order_hash)
-    orderhash_bytes = hexstring_to_bytes(order_hash)
-    msg = orderhash_bytes
-    message_hash = defunct_hash_message(primitive=msg)
-    sighex = acct.signHash(message_hash).signature.hex()
-    v, r, s = to_vrs(sighex)
-    #signed_order = copy.copy(order)
-    #print (signed_order)
-    osig = bytes_to_hexstring(bytes([v])) + \
-                              bytes_to_hexstring(r)[2:] + \
-                              bytes_to_hexstring(s)[2:] + \
-                              "03"  # EthSign
-    #print (osig)
-    order["signature"] = osig
-    return order
-    
 
 def submit_order():
   order = request_order()
   order['makerAddress'] = myaddr.lower()
-  #TODO fix
-  order['makerAssetData'] = (0).to_bytes(1, byteorder='big') * 20
-  order['takerAssetData'] = (0).to_bytes(1, byteorder='big') * 20
+  #sig = signature_order(order)
   order = sign_order(order)
-  order_json = order_to_json(order)
-  print (order_json)
+  #order_json = order_to_json(order)
+  print ("submitting !!! >>>>>> ", order_json)
   response = requests.post("https://api.radarrelay.com/v2/order", order_json, timeout=10.0)
   print (response)
   # ERROR
   #  {"code":100,"reason":"Validation failed","validationErrors":
   # [{"field":"makerAssetData","code":1001,"reason":"Incorrect format (Invalid value)"},
   # {"field":"takerAssetData","code":1001,"reason":"Incorrect format (Invalid value)"}]}
-
-def try_submit():    
-    #maker_create()
-    order = request_order()
     
-    sig = signature_order(order)
-    order["signature"] = sig
-    print (order)
-    print ("signature ",sig)
-    response = requests.post("https://api.radarrelay.com/v2/orders", order, timeout=10.0)
-    print (response.text)
-    
+def submit_example():    
+    example_order = {
+             'makerAddress': "0x0000000000000000000000000000000000000000",
+             'takerAddress': "0x0000000000000000000000000000000000000000",
+             'feeRecipientAddress': "0x0000000000000000000000000000000000000000",
+             'senderAddress': "0x0000000000000000000000000000000000000000",
+             'makerAssetAmount': 1000000000000000000,
+             'takerAssetAmount': 1000000000000000000,
+             'makerFee': 0,
+             'takerFee': 0,
+             'expirationTimeSeconds': 12345,
+             'salt': 12345,
+             'makerAssetData': "0x0000000000000000000000000000000000000000",
+             'takerAssetData': "0x0000000000000000000000000000000000000000",
+             'exchangeAddress': "0x0000000000000000000000000000000000000000",
+    }
+    print (example_order)
+    order_struct = jsdict_order_to_struct(example_order)    
+    sig = sign_order(order_struct)
+    js_order["signature"] = sig
+    js_order = order_to_jsdict(order)
+    print (js_order)
+    #jsdict["makerAssetData"] = "0x" + order["makerAssetData"].hex()
 
 if __name__=='__main__':
-    try_submit()
-
-
-
-
+    #request_order()
+    submit_example()
 
